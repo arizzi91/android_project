d6a295a9b26b660c8709fed2e08e36c5cc211f42
arizzi91 <a.rizzi0108@gmail.com>
2017-09-27 11:41:01 +0200

    Add AppIntro library

---
+++ app/src/main/java/com/example/angelo/myapplication/IntroActivity.java
@@ -0,0 +1,19 @@
+package com.example.angelo.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+
+/**
+ * Created by angelo on 27/09/17.
+ */
+
+public class IntroActivity extends AppIntro {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
